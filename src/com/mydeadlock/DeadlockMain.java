8e17265418be3f9d89761c6ced53f64ec442eafb
nikhil <nikhilesh.soni@timesinternet.in>
2017-02-06 11:12:53 +0530

    added deadlock

--- src/com/mydeadlock/DeadlockMain.java
+++ src/com/mydeadlock/DeadlockMain.java
@@ -6,7 +6,13 @@ package com.mydeadlock;
 public class DeadlockMain {
 
     public static  void main (String[] args){
+        Object lock1 = new Object();
+        Object lock2 =new Object();
+        MyThread t1 =new MyThread(lock1,lock2);
+        MyThread t2 =new MyThread(lock2,lock1);
 
+        t1.start();
+        t2.start();
     }
 
 
--- src/com/mydeadlock/MyThread.java
+++ src/com/mydeadlock/MyThread.java
@@ -3,21 +3,29 @@ package com.mydeadlock;
 /**
  * Created by Nikhilesh.Soni on 31-01-2017.
  */
-public class MyThread implements Runnable {
-    private Object test = new Object();
-    private Object test2 = new Object();
+public class MyThread extends Thread{
+    private Object test;
+    private Object test2;
 
+    public MyThread(Object test, Object test2) {
+        this.test = test;
+        this.test2 = test2;
+    }
 
     @Override
     public void run() {
-        synchronized (test){
-            test2.notifyAll();
-            try {
-                test.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        synchronized (test) {
+            System.out.println("Entered l1 by" +Thread.currentThread().getName());
+            synchronized (test2) {
+                System.out.println("Entered l2 by" +Thread.currentThread().getName());
+                try {
+                    test2.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("lock up");
+
             }
         }
-
     }
 }
